5e741184f0b277ec7b0a6fbba128873ef2df8783
IvanZhantalay <38077983+IvanZhantalay@users.noreply.github.com>
2021-07-06 00:40:44 +0300

    fourth HomeWork

--- 05-task.js
+++ 05-task.js
@@ -1,6 +1,29 @@
-// Задание:
-// Преобразуйте регистр первого символа строки из нижнего регистра в верхний.
-const str = "string not starting with capital";
-const result = str.charAt(0).toUpperCase() + str.slice(1); // тут ваш код вместо null
+//Задание:
+//Найдите и исправьте логические ошибки в сценарии.
 
-console.log(result); // "String not starting with capital"
+let product1 = "Бумага офисная А4, 80 г/м2, 500 л";
+let product2 = "Биндеры для бумаги 51 мм";
+let product3 = "Ручка шариковая синяя";
+
+let productPrice1 = 280.25;
+let productPrice2 = 56;
+let productPrice3 = 12.50;
+
+let productQuantity1 = +prompt(`Укажите количество продуктов '${product1}', цена ${productPrice1}`, 0);
+let productQuantity2 = +prompt(`Укажите количество продуктов '${product2}', цена ${productPrice2}`, 0);
+let productQuantity3 = +prompt(`Укажите количество продуктов '${product3}', цена ${productPrice3}`, 0);
+
+// Выведите на экран сколько пользователю нужно заплатить за каждый продукт по
+// отдельности и всего за все продукты
+// используйте форматированный вывод
+let totalPriceProduct1 = productPrice1 * productQuantity1;
+let totalPriceProduct2 = productPrice2 * productQuantity2;
+let totalPriceProduct3 = productPrice3 * productQuantity3;
+
+let total = totalPriceProduct1 + totalPriceProduct2 + totalPriceProduct3;
+console.log(`Общая сумма по продукту '${product1}' составляет ${totalPriceProduct1.toFixed(2)}`);
+console.log(`Общая сумма по продукту '${product2}' составляет ${totalPriceProduct2.toFixed(2)}`);
+console.log(`Общая сумма по продукту '${product3}' составляет ${totalPriceProduct3.toFixed(2)}`);
+console.log(`Всего ${total.toFixed(2)}`);
+
+
